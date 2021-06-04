8ee6878a59fe32c202a51545d86c5bae83ed7a99
Sunil Kumar <skumar.mobiledev@gmail.com>
2021-06-04 12:49:01 +0530

    Initial Commit

---
+++ SampleIOSApp/SampleIOSAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleIOSAppApp.swift
+//  SampleIOSApp
+//
+//  Created by Sunil Kumar on 04/06/21.
+//
+
+import SwiftUI
+
+@main
+struct SampleIOSAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
